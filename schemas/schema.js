35fafceb666167d56ed37c135e47d080f7b85882
yyt9n <h.yyt9n@gmail.com>
2022-10-21 07:34:23 +0900

    sanity types

--- schemas/schema.js
+++ schemas/schema.js
@@ -6,6 +6,32 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
+  // We name our schema
   name: 'default',
-  types: schemaTypes.concat([]),
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    {
+      name: 'blog',
+      type: 'document',
+      title: 'Blog',
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title'
+        },
+        {
+          name: 'subtitle',
+          type: 'string',
+          title: 'Subtitle'
+        },
+        {
+          name: 'slug',
+          type: 'slug',
+          title: 'Slug'
+        }
+      ]
+    }
+  ])
 })
